226666bc6229d4a58915f9e9feadcdfaae616cb3
stanleykosi <stanleykosi@example.com>
2025-06-28 15:12:29 +0100

    Initial commit - Complete cipher-stratego project with proper structure

---
+++ packages/program/programs/program/src/lib.rs
@@ -0,0 +1,181 @@
+use anchor_lang::prelude::*;
+use arcium_anchor::{
+    comp_def_offset,
+    derive_cluster_pda,
+    derive_comp_def_pda,
+    derive_comp_pda,
+    derive_execpool_pda,
+    derive_mempool_pda,
+    derive_mxe_pda,
+    init_comp_def,
+    queue_computation,
+    ComputationOutputs,
+    ARCIUM_CLOCK_ACCOUNT_ADDRESS,
+    ARCIUM_STAKING_POOL_ACCOUNT_ADDRESS,
+    CLUSTER_PDA_SEED,
+    COMP_PDA_SEED,
+    COMP_DEF_PDA_SEED,
+    EXECPOOL_PDA_SEED,
+    MEMPOOL_PDA_SEED,
+    MXE_PDA_SEED,
+};
+use arcium_client::idl::arcium::{
+    accounts::{
+        ClockAccount, Cluster, ComputationDefinitionAccount, PersistentMXEAccount, StakingPoolAccount
+    },
+    program::Arcium,
+    types::Argument,
+    ID_CONST as ARCIUM_PROG_ID,
+};
+use arcium_macros::{
+    arcium_callback,
+    arcium_program,
+    callback_accounts,
+    init_computation_definition_accounts,
+    queue_computation_accounts,
+};
+
+const COMP_DEF_OFFSET_ADD_TOGETHER: u32 = comp_def_offset("add_together");
+
+declare_id!("5UejADLz4JjiCDqYqDh4xZubzcTjPdZw7fSqvQq9wBjK");
+
+#[arcium_program]
+pub mod program {
+    use super::*;
+
+    pub fn init_add_together_comp_def(ctx: Context<InitAddTogetherCompDef>) -> Result<()> {
+        init_comp_def(ctx.accounts, true, None, None)?;
+        Ok(())
+    }
+
+    pub fn add_together(
+        ctx: Context<AddTogether>,
+        computation_offset: u64,
+        ciphertext_0: [u8; 32],
+        ciphertext_1: [u8; 32],
+        pub_key: [u8; 32],
+        nonce: u128,
+    ) -> Result<()> {
+        let args = vec![
+            Argument::ArcisPubkey(pub_key),
+            Argument::PlaintextU128(nonce),
+            Argument::EncryptedU8(ciphertext_0),
+            Argument::EncryptedU8(ciphertext_1),
+        ];
+        queue_computation(ctx.accounts, computation_offset, args, vec![], None)?;
+        Ok(())
+    }
+
+    #[arcium_callback(encrypted_ix = "add_together")]
+    pub fn add_together_callback(
+        ctx: Context<AddTogetherCallback>,
+        output: ComputationOutputs,
+    ) -> Result<()> {
+        let bytes = if let ComputationOutputs::Bytes(bytes) = output {
+            bytes
+        } else {
+            return Err(ErrorCode::AbortedComputation.into());
+        };
+
+        emit!(SumEvent {
+            sum: bytes[48..80].try_into().unwrap(),
+            nonce: bytes[32..48].try_into().unwrap(),
+        });
+        Ok(())
+    }
+}
+
+#[queue_computation_accounts("add_together", payer)]
+#[derive(Accounts)]
+#[instruction(computation_offset: u64)]
+pub struct AddTogether<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        address = derive_mxe_pda!()
+    )]
+    pub mxe_account: Account<'info, PersistentMXEAccount>,
+    #[account(
+        mut,
+        address = derive_mempool_pda!()
+    )]
+    /// CHECK: mempool_account, checked by the arcium program.
+    pub mempool_account: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        address = derive_execpool_pda!()
+    )]
+    /// CHECK: executing_pool, checked by the arcium program.
+    pub executing_pool: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        address = derive_comp_pda!(computation_offset)
+    )]
+    /// CHECK: computation_account, checked by the arcium program.
+    pub computation_account: UncheckedAccount<'info>,
+    #[account(
+        address = derive_comp_def_pda!(COMP_DEF_OFFSET_ADD_TOGETHER)
+    )]
+    pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
+    #[account(
+        mut,
+        address = derive_cluster_pda!(mxe_account)
+    )]
+    pub cluster_account: Account<'info, Cluster>,
+    #[account(
+        mut,
+        address = ARCIUM_STAKING_POOL_ACCOUNT_ADDRESS,
+    )]
+    pub pool_account: Account<'info, StakingPoolAccount>,
+    #[account(
+        address = ARCIUM_CLOCK_ACCOUNT_ADDRESS
+    )]
+    pub clock_account: Account<'info, ClockAccount>,
+    pub system_program: Program<'info, System>,
+    pub arcium_program: Program<'info, Arcium>,
+}
+
+#[callback_accounts("add_together", payer)]
+#[derive(Accounts)]
+pub struct AddTogetherCallback<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub arcium_program: Program<'info, Arcium>,
+    #[account(
+        address = derive_comp_def_pda!(COMP_DEF_OFFSET_ADD_TOGETHER)
+    )]
+    pub comp_def_account: Account<'info, ComputationDefinitionAccount>,
+    #[account(address = ::anchor_lang::solana_program::sysvar::instructions::ID)]
+    /// CHECK: instructions_sysvar, checked by the account constraint
+    pub instructions_sysvar: AccountInfo<'info>,
+}
+
+#[init_computation_definition_accounts("add_together", payer)]
+#[derive(Accounts)]
+pub struct InitAddTogetherCompDef<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        mut,
+        address = derive_mxe_pda!()
+    )]
+    pub mxe_account: Box<Account<'info, PersistentMXEAccount>>,
+    #[account(mut)]
+    /// CHECK: comp_def_account, checked by arcium program.
+    /// Can't check it here as it's not initialized yet.
+    pub comp_def_account: UncheckedAccount<'info>,
+    pub arcium_program: Program<'info, Arcium>,
+    pub system_program: Program<'info, System>,
+}
+
+#[event]
+pub struct SumEvent {
+    pub sum: [u8; 32],
+    pub nonce: [u8; 16],
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The computation was aborted")]
+    AbortedComputation,
+}
